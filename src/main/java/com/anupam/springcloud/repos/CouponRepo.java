a2dc24c38ec00e180f3f7af9e020257bf002141f
anupam-170490 <74314156+anupam-170490@users.noreply.github.com>
2020-12-15 23:48:29 +0530

    fff

---
+++ src/main/java/com/anupam/springcloud/repos/CouponRepo.java
@@ -0,0 +1,11 @@
+package com.anupam.springcloud.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.anupam.springcloud.model.Coupon;
+
+public interface CouponRepo extends JpaRepository<Coupon, Long> {
+
+	Coupon findByCode(String code);
+
+}
